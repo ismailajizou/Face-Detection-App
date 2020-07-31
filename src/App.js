e3f111e94cd4903a8daff048d782bcccc3967fc4
unknown <ismailajizoumo@gmail.com>
2020-07-31 19:09:03 +0100

    first change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 
-function App() {
-  return (
+const particlesParams =  {
+ particles: {
+   number: {
+     value: 170,
+     density: {
+       enable: true,
+       value_area:  700
+     }
+   }
+}
+}
+class App extends React.Component {
+constructor(){
+  super();
+  this.state = {
+    input: '',
+  }
+}
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+  onBtnSubmit = () => {
+    console.log('click');
+  }
+
+
+render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Particles 
+        className='particles'
+        params={particlesParams} />
+      <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+        onInputChange={this.onInputChange}
+        onBtnSubmit={this.onBtnSubmit}
+        />
+        {/* <FaceRecognition /> */}
+      
     </div>
   );
+}
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// eslint-disable-next-line
+import tachyons from 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
