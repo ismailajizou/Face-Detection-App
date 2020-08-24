e20f74fb0280709971cdc0eec9bfc4693701b343
unknown <ismailajizoumo@gmail.com>
2020-08-24 11:03:17 +0100

    Updates

---
+++ precache-manifest.5a4777193f0f67789586491b72022453.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de01fb4c802c58a3790aebd11a7b4121",
+    "url": "/face-detection/index.html"
+  },
+  {
+    "revision": "bfba32f2c3f5b650959d",
+    "url": "/face-detection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "14a429c8ad35f35aa8ed",
+    "url": "/face-detection/static/css/main.4acbc49b.chunk.css"
+  },
+  {
+    "revision": "bfba32f2c3f5b650959d",
+    "url": "/face-detection/static/js/2.3fa3e397.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-detection/static/js/2.3fa3e397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a429c8ad35f35aa8ed",
+    "url": "/face-detection/static/js/main.36878caf.chunk.js"
+  },
+  {
+    "revision": "d0e840a67897173a025b",
+    "url": "/face-detection/static/js/runtime-main.7181a703.js"
+  }
+]);
