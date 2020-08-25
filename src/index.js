e6136f9863440cac0c8283d185c40a4f82ad7dc9
unknown <ismailajizoumo@gmail.com>
2020-08-25 23:23:23 +0100

    changing title

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// eslint-disable-next-line
 import tachyons from 'tachyons';
 
 ReactDOM.render( <App />,
@@ -13,4 +12,4 @@ ReactDOM.render( <App />,
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
