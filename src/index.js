a751ae4c50357d90c661d53372b36c61df8661e8
unknown <ismailajizoumo@gmail.com>
2020-08-25 23:52:53 +0100

    fixin warnings

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// eslint-disable-next-line
 import tachyons from 'tachyons';
 
 ReactDOM.render( <App />,
