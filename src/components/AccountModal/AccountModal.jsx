59fe044aa90a2324e79438fa9ed5702c6e73f5bf
unknown <ismailajizoumo@gmail.com>
2021-06-17 23:29:12 +0100

    added the delete account feature

--- src/components/AccountModal/AccountModal.jsx
+++ src/components/AccountModal/AccountModal.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
     Modal,
     ModalOverlay,
@@ -5,24 +6,57 @@ import {
     ModalHeader,
     ModalFooter,
     Button,
+    ModalCloseButton,
   } from "@chakra-ui/react"
 import AccountModalContent from "./AccountModalContent";
+import axios from 'axios';
+import { connect } from 'react-redux';
+import {apiURL} from '../../utils/utils'
+import Toast from '../Toast/Toast';
+import { setCurrentUser } from '../../redux/user/user-actions';
 
-const AccountModal = ({ isOpen, onClose }) => {
+const AccountModal = ({ isOpen, onClose, currentUser: {id}, dispatch }) => {
+  const [deleteMode, setDeleteMode] = useState(false);
+  const [input, setInput] = useState('');
+
+  const handleClose = () => {
+    setDeleteMode(false);
+    onClose();
+  }
+
+  const handleClick = async (e) => {
+    if(!deleteMode){
+      setDeleteMode(true);
+    } else {
+      try {
+        const { data } = await axios.post(`${apiURL}/deleteAccount/${id}`, {password: input});
+        if(data.success){
+          dispatch(setCurrentUser(null));
+          Toast('Success', 'success', data.msg);
+        }
+      } catch (err) {
+        Toast('Error occured', 'error', err.response.data.msg);
+      }
+    }
+  }
+
     return ( 
-        <Modal isOpen={isOpen} onClose={onClose}>
+        <Modal isOpen={isOpen} onClose={() => handleClose()}>
           <ModalOverlay />
           <ModalContent>
 
             <ModalHeader textAlign='center' fontSize='3xl' fontWeight='bold'>My Account</ModalHeader>
-            <AccountModalContent />
+            <ModalCloseButton />
+            <AccountModalContent deleteMode={deleteMode} setDeleteMode={setDeleteMode} handleChange={setInput} />
 
             <ModalFooter>
-              <Button colorScheme="blue" mr={3} onClick={onClose}>Close</Button>
+              <Button colorScheme="red" mr={3} onClick={(e) => handleClick(e)}>Delete account</Button>
             </ModalFooter>
           </ModalContent>
       </Modal>
     );
 }
+
+const mapStateToProps = ({user: {currentUser}}) => ({currentUser});
  
-export default AccountModal;
+export default connect(mapStateToProps)(AccountModal);
--- src/components/AccountModal/AccountModalContent.jsx
+++ src/components/AccountModal/AccountModalContent.jsx
@@ -3,22 +3,36 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCurrentUser } from '../../redux/user/user-selectors';
 
-const AccountModalContent = ({ currentUser: { name, email, joined, avatar } }) => {
+const AccountModalContent = ({ currentUser: { name, email, joined, avatar }, deleteMode, handleChange  }) => {
     return ( 
         <ModalBody display='flex' alignItems='center' flexDirection='column'>
-            <Avatar src={avatar} size="xl" mb="5" />
-            <FormControl display='flex' justifyContent='space-between' alignItems='center' mb='5'>
-                <FormLabel m="0">Username</FormLabel>
-                <Input value={name} w="18rem" isReadOnly />
-            </FormControl>
-            <FormControl display='flex' justifyContent='space-between' alignItems='center' mb='5'>
-            <FormLabel m="0">Email</FormLabel>
-                <Input value={email} w="18rem" isReadOnly />
-            </FormControl>
-            <Box textAlign='center' my='4'>
-                <Text fontSize='xl' fontWeight='bold' color='gray'>You're member since :</Text>
-                <Text fontSize='xl' fontWeight='bold' color='gray'>{new Date(joined).toDateString()}</Text>
-            </Box>
+        {
+            !deleteMode ?
+            (
+                <>
+                    <Avatar src={avatar} size="xl" mb="5" />
+                    <FormControl display='flex' justifyContent='space-between' alignItems='center' mb='5'>
+                        <FormLabel m="0">Username</FormLabel>
+                        <Input value={name} w="18rem" isReadOnly />
+                    </FormControl>
+                    <FormControl display='flex' justifyContent='space-between' alignItems='center' mb='5'>
+                    <FormLabel m="0">Email</FormLabel>
+                        <Input value={email} w="18rem" isReadOnly />
+                    </FormControl>
+                    <Box textAlign='center' my='4'>
+                        <Text fontSize='xl' fontWeight='bold' color='gray'>You're member since :</Text>
+                        <Text fontSize='xl' fontWeight='bold' color='gray'>{new Date(joined).toDateString()}</Text>
+                    </Box>
+                </>
+            )
+            :
+            (
+                <>
+                    <Text m="3">To delete this Account you must enter your password and hit the delete button.</Text>
+                    <Input placeholder='Enter you password' type="password" onChange={(e) => handleChange(e.target.value)} />
+                </>
+            )
+        }
         </ModalBody>
      );
 }
